1c771dde802249baddfc1d22fcfa4cb95e27ca72
Your Personal Name <ashokchavala46@gmail.com>
2026-07-04 18:36:34 +0530

    Connected to Mongodb Atlas

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const dbURI = process.env.MONGO_URI;
+// const dbURI = process.env.MONGO_URI;
+const dbURI = process.env.MONGODB_URI;
 
 mongoose
   .connect(dbURI)
