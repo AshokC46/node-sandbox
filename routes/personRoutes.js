eb6a106cb60b6aa748d8a7261f2641afececc764
Your Personal Name <ashokchavala46@gmail.com>
2026-07-08 15:49:06 +0530

    Sign up routes created

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,116 +1,147 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const Person = require('./../models/Person');
+const {jwtAuthMiddleware, generateToken} = require('./../jwt');
 
-const Person = require("../models/person.js");
+// POST route to add a person
+router.post('/signup', async (req, res) =>{
+    try{
+        const data = req.body // Assuming the request body contains the person data
 
-router.get("/", async function (req, res) {
-  try {
-    const data = await Person.find();
-    console.log("Data fetched successfully!");
-    res.status(200).json(data);
-  } catch (err) {
-    console.error("Error fetching data:", err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+        // Create a new Person document using the Mongoose model
+        const newPerson = new Person(data);
 
-router.get("/:workType", async function (req, res) {
-  try {
-    const workType = req.params.workType;
+        // Save the new person to the database
+        const response = await newPerson.save();
+        console.log('data saved');
 
-    if (
-      workType === "Chef" ||
-      workType === "Waiter" ||
-      workType === "Manager" ||
-      workType === "Developer"
-    ) {
-      const filteredData = await Person.find({ work: workType });
-      console.log(`${workType} list fetched successfully!`);
-      return res.status(200).json(filteredData);
-    } else {
-      return res.status(400).json({ error: "Invalid work type specified" });
+        const payload = {
+            id: response.id,
+            username: response.username
+        }
+        console.log(JSON.stringify(payload));
+        const token = generateToken(payload);
+        console.log("Token is : ", token);
+
+        res.status(200).json({response: response, token: token});
     }
-  } catch (err) {
-    console.error("Error fetching data:", err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-// router.get("/:id", async function (req, res) {
-//   try {
-//     const personId = req.params.id;
-//     const response = await Person.findById(personId);
-
-//     if (!response) {
-//       return res.status(404).json({ error: "Person not found" });
-//     }
-
-//     res.status(200).json(response);
-//   } catch (err) {
-//     console.error("Error fetching person:", err);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
-router.post("/", async function (req, res) {
-  try {
-    const data = req.body;
-
-    const newPerson = new Person(data);
-    const response = await newPerson.save();
-
-    console.log("Data saved Successfully");
-    res.status(201).json(response);
-  } catch (err) {
-    console.error("Error saving person:", err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-router.put("/:id", async function (req, res) {
-  try {
-    const personId = req.params.id;
-    const updatedPersonData = req.body;
-
-    const response = await Person.findByIdAndUpdate(
-      personId,
-      updatedPersonData,
-      {
-        returnDocument: "after",
-        runValidators: true,
-      },
-    );
-
-    if (!response) {
-      return res.status(404).json({ error: "Person not found" });
+    catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
     }
+})
 
-    console.log("Data updated successfully!");
-    res.status(200).json(response);
-  } catch (err) {
-    console.error("Error updating person:", err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+// Login Route
+router.post('/login', async(req, res) => {
+    try{
+        // Extract username and password from request body
+        const {username, password} = req.body;
 
-router.delete("/:id", async function (req, res) {
-  try {
-    const personId = req.params.id;
+        // Find the user by username
+        const user = await Person.findOne({username: username});
 
-    const response = await Person.findByIdAndDelete(personId);
+        // If user does not exist or password does not match, return error
+        if( !user || !(await user.comparePassword(password))){
+            return res.status(401).json({error: 'Invalid username or password'});
+        }
 
-    if (!response) {
-      return res.status(404).json({ error: "Person not found" });
+        // generate Token 
+        const payload = {
+            id: user.id,
+            username: user.username
+        }
+        const token = generateToken(payload);
+
+        // resturn token as response
+        res.json({token})
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-
-    console.log("Data deleted successfully!");
-    res
-      .status(200)
-      .json({ message: "Person deleted successfully from the server" });
-  } catch (err) {
-    console.error("Error deleting person:", err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
 });
+
+// Profile route
+router.get('/profile', jwtAuthMiddleware, async (req, res) => {
+    try{
+        const userData = req.user;
+        console.log("User Data: ", userData);
+
+        const userId = userData.id;
+        const user = await Person.findById(userId);
+
+        res.status(200).json({user});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+
+router.get('/', jwtAuthMiddleware, async (req, res) =>{
+    try{
+        const data = await Person.find();
+        console.log('data fetched');
+        res.status(200).json(data);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
+
+router.get('/:workType', async(req, res)=>{
+    try{
+        const workType = req.params.workType; 
+        if(workType == 'chef' || workType == 'manager' || workType == 'waiter' ){
+            const response = await Person.find({work: workType});
+            console.log('response fetched');
+            res.status(200).json(response);
+        }else{
+            res.status(404).json({error: 'Invalid work type'});
+        }
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
+
+router.put('/:id', async (req, res)=>{
+    try{
+        const personId = req.params.id; 
+        const updatedPersonData = req.body; 
+
+        const response = await Person.findByIdAndUpdate(personId, updatedPersonData, {
+            new: true, 
+            runValidators: true, 
+        })
+
+        if (!response) {
+            return res.status(404).json({ error: 'Person not found' });
+        }
+
+        console.log('data updated');
+        res.status(200).json(response);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try{
+        const personId = req.params.id; 
+        
+        
+        const response = await Person.findByIdAndRemove(personId);
+        if (!response) {
+            return res.status(404).json({ error: 'Person not found' });
+        }
+        console.log('data delete');
+        res.status(200).json({message: 'person Deleted Successfully'});
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
+
 
 module.exports = router;
